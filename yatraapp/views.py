c495b1241d7629f21efa876f35473c6f6887c6cd
Neha Pokharel <nehapokharel008@gmail.com>
2019-05-13 16:40:18 +0545

    base page created

---
+++ yatraapp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class BaseView(TemplateView):
+    template_name = 'base.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['active'] = 'base'
+
+        return context
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['active'] = 'home'
+
+        return context
