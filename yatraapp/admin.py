11a278e316ba30251df561439f414647c3189f15
Neha Pokharel <nehapokharel008@gmail.com>
2019-05-14 12:54:29 +0545

    models created for food, festival and events

--- yatraapp/admin.py
+++ yatraapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Food, Festival, Event
 
-# Register your models here.
+admin.site.register(Food)
+
+admin.site.register(Festival)
+
+admin.site.register(Event)
--- yatraapp/models.py
+++ yatraapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from month.models import MonthField
+import datetime
+from django.utils import timezone
 
-# Create your models here.
+
+class Food(models.Model):
+    location = models.CharField(max_length = 50)
+    food_name = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 200)
+    category = models.CharField(max_length = 500)
+    month = MonthField("Month Value")
+
+
+class Festival(models.Model):
+    location = models.CharField(max_length=50)
+    festival_name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    ethinic = models.CharField(max_length=100)
+    month = MonthField("Month Value")
+
+class Event(models.Model):
+    location = models.CharField(max_length=50)
+    event_name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    month = MonthField("Month Value")
+    date_created = models.DateTimeField(default=timezone.now)
+
+
